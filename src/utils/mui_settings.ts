7403aea719083f5c31669db7dd9603c1a2499c11
Taha HALIM <mohamedtahahalim@gmail.com>
2025-09-15 23:17:22 +0100

    initial setup

---
+++ src/utils/mui_settings.ts
@@ -0,0 +1,37 @@
+import type { PaletteOptions } from "@mui/material";
+
+export const lightPalette: PaletteOptions = {
+  primary: {
+    main: "#4A637D",
+    contrastText: "#D1D5DB",
+  },
+  secondary: {
+    main: "#4D6EC2",
+    contrastText: "#e0e0e0",
+  },
+  info: {
+    main: "rgb(77,110,194, 0.3)",
+    contrastText: "#000",
+  },
+  success: {
+    main: "#66BB6A",
+  },
+};
+
+export const darkPalette: PaletteOptions = {
+  primary: {
+    main: "#7895B1",
+    contrastText: "#D1D5DB",
+  },
+  secondary: {
+    main: "#FF6B6B",
+    contrastText: "#ecf0f1",
+  },
+  info: {
+    main: "rgb(255,107,107, 0.15)",
+    contrastText: "#fff",
+  },
+  success: {
+    main: "#E56448",
+  },
+};
